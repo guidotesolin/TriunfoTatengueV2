3fe79051b33f7d1fd5f36f790f1fbed1d388f5cd
Guido Tesolin <60260450+guidotesolin@users.noreply.github.com>
2022-02-20 14:44:32 -0300

    Create empty emailJs file

---
+++ src/components/utils/emailJsConfig.js
@@ -0,0 +1,12 @@
+// Replace with the data fetched from https://www.emailjs.com/
+
+const getConfig = () => {
+  const config = {
+    serviceID: "",
+    templateID: "",
+    userID: "",
+  };
+  return config;
+};
+
+export default getConfig;
